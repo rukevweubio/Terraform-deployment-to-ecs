e666e1470cf8a178f5e59b28828efeab67169bcd
rukevweubio22 <ubiorukevwe@gmail.com>
2025-08-10 13:32:48 +0000

    update the terraform script and github action

--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -8,7 +8,7 @@ variable "ami" {
     default = "ami-0c55b159cbfafe1f0"  # Example AMI ID for us-east-1, update as needed
 }
 variable "vpc_cidr_block" {
-    vpc_id = "10.0.0.0/16"  # Replace with your VPC ID
+    default = "10.0.0.0/16"  # Replace with your VPC ID
 }
 
 variable "public_subnet_cidr_block_a" {
