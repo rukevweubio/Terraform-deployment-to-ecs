b083b32d48a0ac56d9d6aa6197e7e45af2e242a1
rukevweubio22 <ubiorukevwe@gmail.com>
2025-08-10 13:40:39 +0000

    update the script

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,23 +1,35 @@
-output "load_balancer_dns" {
-    value = aws_lb.app_lb.dns_name
-}
-
-
-
-output "ecr_repository_url" {
-    description = "URL of the ECR repository"
-    value       = aws_ecr_repository.app_repo.repository_url
-}
 
 output "ecs_cluster_name" {
-    description = "Name of the ECS cluster"
-    value       = aws_ecs_cluster.app.name
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
 }
-output "ecs_service_name" {
-    description = "Name of the ECS service"
-    value       = aws_ecs_service.app_service.name
+
+output "ecs_task_execution_role_arn" {
+  description = "ARN of the ECS task execution IAM role"
+  value       = aws_iam_role.ecs_task_execution_role.arn
 }
+
+output "load_balancer_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.app_lb.dns_name
+}
+
+output "load_balancer_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.app_lb.arn
+}
+
+output "target_group_arn" {
+  description = "ARN of the Load Balancer target group"
+  value       = aws_lb_target_group.app_tg.arn
+}
+
 output "ecs_task_definition_arn" {
-    description = "ARN of the ECS task definition"
-    value       = aws_ecs_task_definition.app_task_def.arn
+  description = "ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.app.arn
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.app.name
 }
